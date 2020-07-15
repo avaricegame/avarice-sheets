d59f73a051d503fe2e1763695875b5ce7ecd272e
Talon Durrant <talonmdurrant@gmail.com>
2020-07-14 22:35:16 -0600

    finished info for now

--- src/Components/CharacterSheet/Info.js
+++ src/Components/CharacterSheet/Info.js
@@ -1,70 +1,313 @@
 import React from "react"
 
-// // IMPORT ALL THE FORM COMPONENTS
-// import CharacterDescriptionForm from "../../../Forms/CharacterDescriptionForm"
-// import CharacterBackgroundForm from "../../../Forms/CharacterBackgroundForm"
+function Info() {
+  return (
+    <>
+      <div className="secondary-header">
+        <h1 className="secondary-header__heading">Info</h1>
+      </div>
+      <div className="page-container">
+        <div className="cw__33">
+          <h2 className="heading">Campaign Information</h2>
+          <div className="cw__container">
+            <button>Add a New Character Log</button>
 
-// // IMPORT ALL OF THE MAPPING COMPONENTS
-// import CharacterDescription from "./Maps/CharacterDescription"
-// import CharacterBackground from "./Maps/CharacterBackground"
-// import RaceDetails from "./Maps/RaceDetails"
-// import ClassDetails from "./Maps/ClassDetails"
+            <div className="item-container">
+              <h3 className="item-container__heading">Character Log</h3>
+              <h4 className="item-container__subheading">For: Fluttering Butterflies Campaign</h4>
+              <h4 className="item-container__terheading">For: Wayne Bradford</h4>
+              <p>
+                <strong>Mission III: </strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+              </p>
+              <p>
+                <strong>Mission II: </strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+              </p>
+              <p>
+                <strong>Mission I: </strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+              </p>
+            </div>
 
-// BEGIN CLASS
-
-class Info extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="secondary-header">
-          <h1>Info</h1>
-        </div>
-
-        {/****** BEGIN CAMPAIGN INFO SECTION ******/}
-        <div className="campaign-details bgb">
-          <div className="heading-bar">
-            <h2>Campaign Details</h2>
+            <div className="item-container">
+              <h3 className="item-container__heading">Master Log</h3>
+              <h4 className="item-container__subheading">For: Fluttering Butterflies Campaign</h4>
+              <p>
+                <strong>Mission III: </strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+              </p>
+              <p>
+                <strong>Mission II: </strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+              </p>
+              <p>
+                <strong>Mission I: </strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+              </p>
+            </div>
           </div>
-          <p>The Campaign functionality is currently under construction, but will be available soon!</p>
+
+          <div className="item-container">
+            <h3 className="item-container__heading">Campaign Details</h3>
+            <h4 className="item-container__subheading">For: Fluttering Butterflies Campaign</h4>
+            <p>
+              <strong>Background: </strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+            </p>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Campaign Name:</td>
+                  <td>Fluttering Butterflies</td>
+                </tr>
+                <tr>
+                  <td>Campaign ID:</td>
+                  <td>#123ABC</td>
+                </tr>
+                <tr>
+                  <td>Players:</td>
+                  <td>Player 1, Player 2, Player 3</td>
+                </tr>
+                <tr>
+                  <td>Characters:</td>
+                  <td>Character 1, Character 2, Character 3</td>
+                </tr>
+                <tr>
+                  <td>Missions Served:</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="cw__33">
+          <h2 className="heading">Race and Class Information</h2>
+          <div className="cw__container">
+            <div className="item-container">
+              <h3 className="item-container__heading">Class Details and Information</h3>
+              <h4 className="item-container__subheading">For: Wayne Bradford</h4>
+              <h4 className="item-container__terheading">Apa Class</h4>
+              <p>
+                <strong>Quote: </strong>
+                <em>Here is the quote for the class</em>
+              </p>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Class Name:</td>
+                    <td>Apa</td>
+                  </tr>
+                  <tr>
+                    <td>Common Name:</td>
+                    <td>Aquamancer</td>
+                  </tr>
+                  <tr>
+                    <td>Special Abilities:</td>
+                    <td>
+                      <em>See Abilities Tab</em>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <table>
+                <tbody></tbody>
+                <tbody>
+                  <tr>
+                    <td>
+                      <strong>Ability Scores:</strong>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Physique:</td>
+                    <td>4</td>
+                  </tr>
+                  <tr>
+                    <td>Reflex:</td>
+                    <td>4</td>
+                  </tr>
+                  <tr>
+                    <td>Intelligence:</td>
+                    <td>4</td>
+                  </tr>
+                  <tr>
+                    <td>Charisma:</td>
+                    <td>4</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="item-container">
+              <h3 className="item-container__heading">Race Details and Information</h3>
+              <h4 className="item-container__subheading">For: Wayne Bradford</h4>
+              <h4 className="item-container__terheading">Bruttu Race</h4>
+              <p>
+                <strong>Description: </strong>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <table>
+                <tr>
+                  <td>Race Name:</td>
+                  <td>Bruttu</td>
+                </tr>
+                <tr>
+                  <td>Common Name:</td>
+                  <td>Orc</td>
+                </tr>
+                <tr>
+                  <td>Location:</td>
+                  <td>undefined</td>
+                </tr>
+                <tr>
+                  <td>Climate:</td>
+                  <td>undefined</td>
+                </tr>
+                <tr>
+                  <td>Size:</td>
+                  <td>undefined</td>
+                </tr>
+                <tr>
+                  <td>Lifespan:</td>
+                  <td>undefined</td>
+                </tr>
+                <tr>
+                  <td>Personality:</td>
+                  <td>undefined</td>
+                </tr>
+                <tr>
+                  <td>Friends:</td>
+                  <td>undefined</td>
+                </tr>
+                <tr>
+                  <td>Enemies:</td>
+                  <td>undefined</td>
+                </tr>
+              </table>
+              <table>
+                <tr>
+                  <td>
+                    <strong>Ability Scores:</strong>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Physique:</td>
+                  <td>4</td>
+                </tr>
+                <tr>
+                  <td>Reflex:</td>
+                  <td>4</td>
+                </tr>
+                <tr>
+                  <td>Intelligence:</td>
+                  <td>4</td>
+                </tr>
+                <tr>
+                  <td>Charisma:</td>
+                  <td>4</td>
+                </tr>
+              </table>
+            </div>
+          </div>
         </div>
 
-        {/****** BEGIN CHARACTER INFO SECTION ******/}
-        <div className="character-details bgb">
-          <div className="heading-bar">
-            <h2>Character Background and Information</h2>
-          </div>
-          {/* <CharacterDescriptionForm /> */}
-          {/* <CharacterDescription characterSheet={characterSheet} /> */}
-          {/* <CharacterBackgroundForm /> */}
-          {/* <CharacterBackground characterSheet={characterSheet} /> */}
-        </div>
+        <div className="cw__33">
+          <h2 className="heading">Character Information</h2>
+          <div className="cw__container">
+            <div className="item-container">
+              <h3 className="item-container__heading">Character Description</h3>
+              <h4 className="item-container__subheading">For: Wayne Bradford</h4>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Age:</td>
+                    <td>undefined years old</td>
+                  </tr>
+                  <tr>
+                    <td>Weight:</td>
+                    <td>undefined lbs.</td>
+                  </tr>
+                  <tr>
+                    <td>Height:</td>
+                    <td>undefined ft.</td>
+                  </tr>
+                  <tr>
+                    <td>Gender:</td>
+                    <td>undefined</td>
+                  </tr>
+                  <tr>
+                    <td>Skin Tone:</td>
+                    <td>undefined</td>
+                  </tr>
+                  <tr>
+                    <td>Eye Color:</td>
+                    <td>undefined</td>
+                  </tr>
+                  <tr>
+                    <td>Hair Color:</td>
+                    <td>undefined</td>
+                  </tr>
+                  <tr>
+                    <td>Hair Length:</td>
+                    <td>undefined</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="item-container">
+              <h3 className="item-container__heading">Character Background</h3>
+              <h4 className="item-container__subheading">For: Wayne Bradford</h4>
+              <p>
+                <strong>Who were your parents? How were you raised? Do you have any close family?</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
 
-        {/****** BEGIN RACE INFO SECTION ******/}
-        <div className="race-details bgb">
-          <div className="heading-bar">
-            <h2>Race Details and Information</h2>
-          </div>
-          {/* <RaceDetails races={races} characterSheet={characterSheet} /> */}
-        </div>
+              <p>
+                <strong>Where/From whom did you learn your skills?</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
 
-        {/****** BEGIN CLASS INFO SECTION ******/}
-        <div className="class-details bgb">
-          <div className="heading-bar">
-            <h2>Class Details and Information</h2>
-          </div>
-          {/* <ClassDetails classes={classes} characterSheet={characterSheet} /> */}
-        </div>
+              <p>
+                <strong>Name your 3 most important personal beliefs:</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
 
-        {/****** BEGIN QUICK RULES SECTION ******/}
-        <div className="quick-rules bgb">
-          <div className="heading-bar">
-            <h2>Quick Rules</h2>
+              <p>
+                <strong>Name 3 or more character flaws that are most likely to trouble you:</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <p>
+                <strong>What do you secretly fear even in times of easy-going peace? Why?</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <p>
+                <strong>Are you (Wayne Bradford) a hero? Why or why not?</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <p>
+                <strong>What are your goals (both long and short term)?</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <p>
+                <strong>Are you (Wayne Bradford) merciful? Why or why not?</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <p>
+                <strong>Why are you an adventurer?</strong>
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
           </div>
-          <p>The quick rules feature is currently under construction but will be done soon!</p>
         </div>
-      </React.Fragment>
-    ) // END RETURN
-  } // END RENDER
-} // END CLASS
+      </div>
+    </>
+  )
+}
 
 export default Info
