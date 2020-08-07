821162a30e131dd71fe72dac47c53a258fd0fcb8
Talon Durrant <talonmdurrant@gmail.com>
2020-08-07 01:34:53 -0600

    after FOREVER I finally got the routing correct to view individual characters sheets based on which was chosen and I added media queries to make the site responsive

--- src/Components/Pages/CampaignSheet.js
+++ src/Components/Pages/CampaignSheet.js
@@ -9,7 +9,7 @@ import Players from "../CampaignSheet/Players"
 import Info from "../CampaignSheet/Info"
 import Planning from "../CampaignSheet/Planning"
 import Campaign from "../CampaignSheet/Campaign"
-import Notes from "../Notes"
+import Notes from "../CampaignSheet/Notes"
 import Messages from "../Messages"
 import Navigation from "../CampaignSheet/Navigation"
 import Home from "../Pages/Home"
@@ -21,58 +21,58 @@ function CampaignSheet() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/character/:id/gameplay">
+        <Route path="/character/gameplay">
           <CharacterSheet />
         </Route>
-        <Route path="/profile/:username">
+        <Route path="/profile/">
           <Profile />
         </Route>
         <Route path="/" exact>
           <Home />
         </Route>
-        <Route path="/campaign/:id/about">
+        <Route path="/campaign/about">
           <Header />
           <Navigation />
           <About />
           <Footer />
         </Route>
-        <Route path="/campaign/:id/gameplay" exact>
+        <Route path="/campaign/gameplay" exact>
           <Header />
           <Navigation />
           <Gameplay />
           <Footer />
         </Route>
-        <Route path="/campaign/:id/players" exact>
+        <Route path="/campaign/players" exact>
           <Header />
           <Navigation />
           <Players />
           <Footer />
         </Route>
-        <Route path="/campaign/:id/info" exact>
+        <Route path="/campaign/info" exact>
           <Header />
           <Navigation />
           <Info />
           <Footer />
         </Route>
-        <Route path="/campaign/:id/planning" exact>
+        <Route path="/campaign/planning" exact>
           <Header />
           <Navigation />
           <Planning />
           <Footer />
         </Route>
-        <Route path="/campaign/:id/campaign" exact>
+        <Route path="/campaign/campaign" exact>
           <Header />
           <Navigation />
           <Campaign />
           <Footer />
         </Route>
-        <Route path="/campaign/:id/messages" exact>
+        <Route path="/campaign/messages" exact>
           <Header />
           <Navigation />
           <Messages />
           <Footer />
         </Route>
-        <Route path="/campaign/:id/notes" exact>
+        <Route path="/campaign/notes" exact>
           <Header />
           <Navigation />
           <Notes />
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,104 +1,135 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import Axios from "axios"
 
 function Home(props) {
-  return (
-    <>
-      <div className="header-bar">
-        <Link to="/profile/:username">
-          <div className="header-bar__profile-button">
-            <img src="/assets/temp-user.jpg" alt="user" className="header-bar__profile-img" />
-          </div>
-        </Link>
-      </div>
-      <header className="header">
-        <h1 className="header__logotype">Avarice Sheets</h1>
-      </header>
-      <div className="container">
-        <div className="campaign-sheets">
-          <h2 className="heading">Character Sheets</h2>
-          <div className="sheets-container">
-            <button>Create a New Character Sheet</button>
-            {props.characterSheetArray.map((cs) => {
-              return (
-                <Link to={`/character/${cs.charid}/gameplay`} key={cs.charid} className="fixing-link-settings">
-                  <div className="item-container item-container--hover-highlight">
-                    <h3 className="item-container__heading">{cs.characterName}</h3>
-                    <h4 className="item-container__subheading">charid: {cs.charid}</h4>
-                    <table>
-                      <tbody>
-                        <tr>
-                          <td>Level:</td>
-                          <td>{cs.level}</td>
-                        </tr>
-                        <tr>
-                          <td>Race:</td>
-                          <td>{cs.raceName}</td>
-                        </tr>
-                        <tr>
-                          <td>Class:</td>
-                          <td>{cs.className}</td>
-                        </tr>
-                        <tr>
-                          <td>Campaign ID:</td>
-                          <td>
-                            <em>{cs.campaignID}</em>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </Link>
-              )
-            })}
-          </div>
-        </div>
-        <div className="character-sheets">
-          <h2 className="heading">Campaign Sheets</h2>
-          <div className="sheets-container">
-            <button>Create a New Campaign</button>
+  const [isLoading, setIsLoading] = useState(true)
+  const [characterSheetArray, setCharacterSheetArray] = useState(0)
 
-            <Link to="/campaign/123/gameplay" className="fixing-link-settings">
-              <div className="item-container item-container--hover-highlight">
-                <h3 className="item-container__heading">Welcoming Treaties</h3>
-                <h4 className="item-container__subheading item-container__subheading--blue">campid: #123abc</h4>
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>Players:</td>
-                      <td>Player 1, Player 2, Player 3</td>
-                    </tr>
-                    <tr>
-                      <td>Characters:</td>
-                      <td>Character 1, Character 2, Character 3</td>
-                    </tr>
-                    <tr>
-                      <td>Missions Served:</td>
-                      <td>5</td>
-                    </tr>
-                    <tr>
-                      <td>Current Mission:</td>
-                      <td>
-                        <em>"Finding Friends"</em>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </Link>
+  useEffect(() => {
+    async function fetchCS() {
+      // get all of the character sheets that match the current user id
+      const corrSheet = await Axios.get("/loadmanycs", {
+        params: {
+          UID: props.UID,
+        },
+      })
+      setCharacterSheetArray(corrSheet.data)
+      setIsLoading(false)
+    }
+    fetchCS()
+  }, [props.UID])
+
+  const clickHandler = (e, id) => {
+    props.CSIDHandler(id)
+  }
+
+  if (!isLoading) {
+    return (
+      <>
+        <div className="header-bar">
+          <Link to="/profile">
+            <div className="header-bar__profile-button">
+              <img src="/assets/temp-user.jpg" alt="user" className="header-bar__profile-img" />
+            </div>
+          </Link>
+        </div>
+        <header className="header">
+          <h1 className="header__logotype">Avarice Sheets</h1>
+        </header>
+        <div className="container">
+          <div className="campaign-sheets">
+            <h2 className="heading">Character Sheets</h2>
+            <div className="sheets-container">
+              <button>Create a New Character Sheet</button>
+              {characterSheetArray.map((cs) => {
+                return (
+                  <Link onClick={(e, id) => clickHandler(e, cs.charid)} to={`/character/gameplay`} key={cs.charid} className="fixing-link-settings">
+                    <div className="item-container item-container--hover-highlight">
+                      <h3 className="item-container__heading">{cs.characterName}</h3>
+                      <h4 className="item-container__subheading">charid: {cs.charid}</h4>
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td>Level:</td>
+                            <td>{cs.level}</td>
+                          </tr>
+                          <tr>
+                            <td>Race:</td>
+                            <td>{cs.raceName}</td>
+                          </tr>
+                          <tr>
+                            <td>Class:</td>
+                            <td>{cs.className}</td>
+                          </tr>
+                          <tr>
+                            <td>Campaign ID:</td>
+                            <td>
+                              <em>{cs.campaignID}</em>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </Link>
+                )
+              })}
+            </div>
+          </div>
+          <div className="character-sheets">
+            <h2 className="heading">Campaign Sheets</h2>
+            <div className="sheets-container">
+              <button>Create a New Campaign</button>
+
+              <Link to="/campaign/gameplay" className="fixing-link-settings">
+                <div className="item-container item-container--hover-highlight">
+                  <h3 className="item-container__heading">Campaign Name</h3>
+                  <h4 className="item-container__subheading item-container__subheading--blue">campid: #exampleid</h4>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Players:</td>
+                        <td>Player 1, Player 2, Player 3</td>
+                      </tr>
+                      <tr>
+                        <td>Characters:</td>
+                        <td>Character 1, Character 2, Character 3</td>
+                      </tr>
+                      <tr>
+                        <td>Missions Served:</td>
+                        <td>#</td>
+                      </tr>
+                      <tr>
+                        <td>Current Mission:</td>
+                        <td>
+                          <em>"Mission Name"</em>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
+        <footer className="footer">
+          <p className="footer__text">
+            &copy; 2020 by Avarice. All rights reserved. Application built by{" "}
+            <a href="https://talonmd.github.io/talondurrant/" target="_blank" rel="noopener noreferrer">
+              Talon Durrant
+            </a>
+          </p>
+        </footer>
+      </>
+    )
+  } else {
+    return (
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100vw", height: "100vh" }}>
+        <h1>Fetching your profile data...</h1>
+        <p>(this one is actually a loading page)</p>
       </div>
-      <footer className="footer">
-        <p className="footer__text">
-          &copy; 2020 by Avarice. All rights reserved. Application built by{" "}
-          <a href="https://talonmd.github.io/talondurrant/" target="_blank" rel="noopener noreferrer">
-            Talon Durrant
-          </a>
-        </p>
-      </footer>
-    </>
-  )
+    )
+  }
 }
 
 export default Home
