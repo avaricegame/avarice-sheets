d932db1f91f0d4f65051bf56d9a67f649e4e1b03
Talon Durrant <talonmdurrant@gmail.com>
2020-08-10 12:41:18 -0600

    brought in proficiency values to be able to properly calculate the damage done

--- src/Components/CharacterSheet/Actions/Attack.js
+++ src/Components/CharacterSheet/Actions/Attack.js
@@ -1,48 +1,64 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 function Attack(props) {
   const [currentWeapon, setCurrentWeapon] = useState("a")
+  const [currentProficiency, setCurrentProficiency] = useState("a")
+  const [proficiency1, setProficiency1] = useState()
+  useEffect(() => {
+    setProficiency1(props.charSheet.levelUps[props.charSheet.levelUps.length - 1].proficiency[0].value)
+  }, [props.charSheet])
   const close = () => {
     props.attackHandler(false)
   }
   const setWeaponHandler = (e) => {
     if (e.target.value !== "a") {
       setCurrentWeapon(props.charSheet.weapons[e.target.value])
+      setCurrentProficiencyHandler(props.charSheet.weapons[e.target.value])
     } else {
       setCurrentWeapon("a")
+    }
+  }
+  const setCurrentProficiencyHandler = (weapon) => {
+    if (weapon.proficiency === "Proficiency 1") {
+      setCurrentProficiency(proficiency1)
     }
   }
   const displayWeapon = () => {
     if (currentWeapon !== "a") {
       return (
-        <div className="current-weapon item-container">
-          <h3 className="item-container__heading">{currentWeapon.name}</h3>
-          <h4 className="item-container__subheading">{currentWeapon.rangedMelee}</h4>
+        <>
+          <div className="current-weapon item-container">
+            <h3 className="item-container__heading">{currentWeapon.name}</h3>
+            <h4 className="item-container__subheading">{currentWeapon.rangedMelee}</h4>
+            <p>
+              <strong>Description: </strong>
+              {currentWeapon.description}
+            </p>
+            <p>
+              <strong>Range: </strong>
+              {currentWeapon.range}
+            </p>
+            <p>
+              <strong>Effects: </strong>
+              {currentWeapon.effects}
+            </p>
+            <p>
+              <strong>Requirements: </strong>
+              {currentWeapon.requirements}
+            </p>
+            <p>
+              <strong>Damage: </strong>
+              {currentWeapon.damage}
+            </p>
+            <p>
+              <strong>Proficiency: </strong>
+              {currentWeapon.proficiency}
+            </p>
+          </div>
           <p>
-            <strong>Description: </strong>
-            {currentWeapon.description}
+            <strong>{`Your proficiency value for ${currentWeapon.proficiency} is ${currentProficiency}.`}</strong>
           </p>
-          <p>
-            <strong>Range: </strong>
-            {currentWeapon.range}
-          </p>
-          <p>
-            <strong>Effects: </strong>
-            {currentWeapon.effects}
-          </p>
-          <p>
-            <strong>Requirements: </strong>
-            {currentWeapon.requirements}
-          </p>
-          <p>
-            <strong>Damage: </strong>
-            {currentWeapon.damage}
-          </p>
-          <p>
-            <strong>Proficiency: </strong>
-            {currentWeapon.proficiency}
-          </p>
-        </div>
+        </>
       )
     }
   }
@@ -51,38 +67,38 @@ function Attack(props) {
       if (currentWeapon.damage === "D4") {
         if (window.confirm(`Your D20 roll was ${Math.ceil(Math.random() * 20)}. Did you hit? Click 'OK' to proceed, and 'Cancel' if your attack was unsuccessful.`)) {
           let num = Math.ceil(Math.random() * 4)
-          alert(`You rolled a D4 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is // temporarily set to 2 //. This brings your total damage done to //${num * 2}//)`)
+          alert(`You rolled a D4 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is ${currentProficiency}. This brings your total damage done to ${currentProficiency ? num * currentProficiency : num}`)
         }
       } else if (currentWeapon.damage === "D6") {
         if (window.confirm(`Your D20 roll was ${Math.ceil(Math.random() * 20)}. Did you hit? Click 'OK' to proceed, and 'Cancel' if your attack was unsuccessful.`)) {
           let num = Math.ceil(Math.random() * 6)
-          alert(`You rolled a D6 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is // temporarily set to 2 //. This brings your total damage done to //${num * 2}//)`)
+          alert(`You rolled a D6 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is ${currentProficiency}. This brings your total damage done to ${currentProficiency ? num * currentProficiency : num}`)
         }
       } else if (currentWeapon.damage === "D8") {
         if (window.confirm(`Your D20 roll was ${Math.ceil(Math.random() * 20)}. Did you hit? Click 'OK' to proceed, and 'Cancel' if your attack was unsuccessful.`)) {
           let num = Math.ceil(Math.random() * 8)
-          alert(`You rolled a D8 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is // temporarily set to 2 //. This brings your total damage done to //${num * 2}//)`)
+          alert(`You rolled a D8 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is ${currentProficiency}. This brings your total damage done to ${currentProficiency ? num * currentProficiency : num}`)
         }
       } else if (currentWeapon.damage === "D10") {
         if (window.confirm(`Your D20 roll was ${Math.ceil(Math.random() * 20)}. Did you hit? Click 'OK' to proceed, and 'Cancel' if your attack was unsuccessful.`)) {
           let num = Math.ceil(Math.random() * 10)
-          alert(`You rolled a D10 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is // temporarily set to 2 //. This brings your total damage done to //${num * 2}//)`)
+          alert(`You rolled a D10 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is ${currentProficiency}. This brings your total damage done to ${currentProficiency ? num * currentProficiency : num}`)
         }
       } else if (currentWeapon.damage === "D12") {
         if (window.confirm(`Your D20 roll was ${Math.ceil(Math.random() * 20)}. Did you hit? Click 'OK' to proceed, and 'Cancel' if your attack was unsuccessful.`)) {
           let num = Math.ceil(Math.random() * 12)
-          alert(`You rolled a D12 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is // temporarily set to 2 //. This brings your total damage done to //${num * 2}//)`)
+          alert(`You rolled a D12 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is ${currentProficiency}. This brings your total damage done to ${currentProficiency ? num * currentProficiency : num}`)
         }
       } else if (currentWeapon.damage === "D20") {
         if (window.confirm(`Your D20 roll was ${Math.ceil(Math.random() * 20)}. Did you hit? Click 'OK' to proceed, and 'Cancel' if your attack was unsuccessful.`)) {
           let num = Math.ceil(Math.random() * 20)
-          alert(`You rolled a D20 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is // temporarily set to 2 //. This brings your total damage done to //${num * 2}//)`)
+          alert(`You rolled a D20 and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is ${currentProficiency}. This brings your total damage done to ${currentProficiency ? num * currentProficiency : num}`)
         }
       } else {
         if (window.confirm(`Your D20 roll was ${Math.ceil(Math.random() * 20)}. Did you hit? Click 'OK' to proceed, and 'Cancel' if your attack was unsuccessful.`)) {
           let number = prompt("How many sided die do you need to roll for damage? Please enter a postive, whole integer.")
           let num = Math.ceil(Math.random() * number)
-          alert(`You rolled a D${number} and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is // temporarily set to 2 //. This brings your total damage done to //${num * 2}//)`)
+          alert(`You rolled a D${number} and got a ${num}! Your proficiency (${currentWeapon.proficiency}) value is ${currentProficiency}. This brings your total damage done to ${currentProficiency ? num * currentProficiency : num}`)
         }
       }
     }
@@ -105,17 +121,6 @@ function Attack(props) {
               })}
             </select>
             {displayWeapon()}
-            {
-              // GET ALL THE PROFICIENCY INFORMATION AND GET IT CALCULATED TO ADD TO THE ROLLS PROPERLY
-            }
-            {/* {props.charSheet.levelUps[props.charSheet.levelUps.length - 1].proficiency.map((prof, index) => {
-              return (
-                <tr key={index}>
-                  <td>{prof.name}</td>
-                  <td>{prof.value}</td>
-                </tr>
-              )
-            })} */}
             <input
               onClick={(e) => {
                 onAttackSubmit(e)
