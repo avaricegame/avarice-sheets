51c86dd75950f5a2c0cf4f017fe03a721b1a9d3c
Talon Durrant <talonmdurrant@gmail.com>
2020-10-24 14:30:16 -0600

    added under construction message

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -6,7 +6,9 @@ function Messages() {
       <div className="secondary-header">
         <h1 className="secondary-header__heading">Messages</h1>
       </div>
-      <div className="page-container">{/* /////// CLOSE PAGE CONTAINER /////// */}</div>
+      <div className="page-container">
+        <h2 style={{ marginLeft: "50%", transform: "translateX(-50%)" }}>MESSAGES FEATURE IS UNDER CONSTRUCTION</h2>
+      </div>
     </>
   )
 }
