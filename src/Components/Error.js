98cd81479650796a6529f765825d2ab427563ea9
Talon Durrant <talonmdurrant@gmail.com>
2020-10-24 14:29:01 -0600

    created error component

---
+++ src/Components/Error.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Error() {
+  return (
+    <div className="fullpage-container">
+      <h1>Oops...</h1>
+      <p></p>
+      <h2>something went wrong</h2>
+      <p></p>
+      <h3>
+        <a href="/">AVARICE SHEETS HOME</a>
+      </h3>
+    </div>
+  )
+}
+
+export default Error
