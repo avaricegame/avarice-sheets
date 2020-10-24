43e18aa825c297cbbf63b05e99fa5e2b72cca7d4
Talon Durrant <talonmdurrant@gmail.com>
2020-10-24 14:29:21 -0600

    changed loader into a component

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -2,13 +2,13 @@ import React from "react"
 
 function Loader() {
   return (
-    <>
+    <div className="fullpage-container">
       <div className="animated-loader">
         <div></div>
         <div></div>
         <div></div>
       </div>
-    </>
+    </div>
   )
 }
 
