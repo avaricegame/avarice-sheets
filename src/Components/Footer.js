530a3aa7bba972d0150f9cb72cadbb27f26519f6
Talon Durrant <talonmdurrant@gmail.com>
2020-11-22 00:45:49 -0700

    finished the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,17 +1,26 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">
-        &copy; 2020 by{" "}
+        <Link to="/about">About</Link>
+        {"   "}|{"   "}
+        <Link to="/terms">Terms</Link>
+        {"   "}|{"   "}
+        <Link to="/privacy">Privacy</Link>
+      </p>
+      <p className="footer__text">
+        &copy; {new Date().getFullYear()} by{" "}
         <a href="https://avarice-website.netlify.app/" rel="noopener noreferrer" target="_blank">
-          Avarice.
-        </a>{" "}
-        All rights reserved. Application built by{" "}
+          Avarice
+        </a>
+        . All rights reserved. Application built by{" "}
         <a href="https://www.talondurrant.com/" target="_blank" rel="noopener noreferrer">
           Talon Durrant
         </a>
+        .
       </p>
     </footer>
   )
