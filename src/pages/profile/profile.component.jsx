7a346d1e274bd3c2799037b3b092105545f200d9
Talon Durrant <talonmdurrant@gmail.com>
2021-01-24 06:45:05 -0700

    set up profile page file

---
+++ src/pages/profile/profile.component.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Header from "../../components/header/header.component"
+import Footer from "../../components/footer/footer.component"
+
+import "./profile.styles.scss"
+
+const ProfilePage = () => (
+  <>
+    <Header />
+    <div>profile page</div>
+    <Footer />
+  </>
+)
+
+export default ProfilePage
