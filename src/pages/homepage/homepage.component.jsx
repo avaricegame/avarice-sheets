2f1d37d60aed8e12197139affca0238db9b898f5
Talon Durrant <talonmdurrant@gmail.com>
2021-01-24 07:37:25 -0700

    finished writing out the homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,49 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
+import Header from "../../components/header/header.component"
+import Footer from "../../components/footer/footer.component"
+
+import CharacterSheetCard from "../../components/character-sheet-card/character-sheet-card.component"
+import CampaignSheetCard from "../../components/campaign-sheet-card/campaign-sheet-card.component"
+
+import CustomButton from "../../components/custom-button/custom-button.component"
 
 import "./homepage.styles.scss"
 
-const HomePage = () => <div>This is the homepage</div>
+class HomePage extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="container">
+          <div className="campaign-sheets">
+            <h2 className="heading">Character Sheets</h2>
+            <div className="sheets-container">
+              <CustomButton>Create a New Character Sheet</CustomButton>
+              <Link className="sheets-card-link" to="/character/12345">
+                <CharacterSheetCard />
+              </Link>
+              <CharacterSheetCard />
+              <CharacterSheetCard />
+            </div>
+          </div>
+          <div className="character-sheets">
+            <h2 className="heading">Campaign Sheets</h2>
+            <div className="sheets-container">
+              <CustomButton>Create a New Campaign Sheet</CustomButton>
+              <Link className="sheets-card-link" to="/campaign/12345">
+                <CampaignSheetCard />
+              </Link>
+              <CampaignSheetCard />
+              <CampaignSheetCard />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </>
+    )
+  }
+}
 
 export default HomePage
