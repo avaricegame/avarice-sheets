acb6bb62754f6225af23a83aa04e78a7350ac9c9
Talon Durrant <talonmdurrant@gmail.com>
2021-01-24 04:12:30 -0700

    getting overall project structure set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import { Switch, Route, Redirect } from "react-router-dom"
+
+import "./App.scss"
+
+import ErrorBoundary from "./components/error-boundary/error-boundary.component"
+
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
+import HomePage from "./pages/homepage/homepage.component"
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  // useEffect(() => {
+  //   setCurrentUser({
+  //     name: "Talon",
+  //     email: "email@email.com",
+  //   })
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Switch>
+        <ErrorBoundary>
+          <Route
+            exact
+            path="/"
+            render={() => (currentUser ? <HomePage /> : <SignInAndSignUpPage />)}
+          />
+        </ErrorBoundary>
+      </Switch>
+    </>
+  )
 }
 
-export default App;
+export default App
