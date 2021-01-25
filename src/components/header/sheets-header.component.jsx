6bc05423815f560da6fe3621594e82952ef07c55
Talon Durrant <talonmdurrant@gmail.com>
2021-01-25 01:43:11 -0700

    making about route dynamic to work with character and campaign sheets

--- src/components/header/sheets-header.component.jsx
+++ src/components/header/sheets-header.component.jsx
@@ -13,7 +13,7 @@ const SheetsHeader = ({ name, character, campaign }) => (
       </div>
     </Link>
     <h1 className="header__logotype">{name}</h1>
-    <Link to={`/character/about`}>
+    <Link to={`/${campaign ? "campaign" : "character"}/about`}>
       <div className="header__about-button">
         <span className="header__about-button-span">?</span>
       </div>
