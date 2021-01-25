704ff0c45fb8c8dacf4183ad70e50d6eb4a450c6
Talon Durrant <talonmdurrant@gmail.com>
2021-01-25 01:37:41 -0700

    styled notes page

--- src/pages/sheets-pages/notes/notes.components.jsx
+++ src/pages/sheets-pages/notes/notes.components.jsx
@@ -7,8 +7,9 @@ import {
   SheetsPageContainerColumnSection as Section,
 } from "../../../components/sheets-page-container-column/sheets-page-container-column.component"
 
-import { default as Card } from "../../../components/card-container/card-container.component"
 import { default as Button } from "../../../components/custom-button/custom-button.component"
+
+import "./notes.styles.scss"
 
 class NotesPage extends React.Component {
   render() {
@@ -19,45 +20,24 @@ class NotesPage extends React.Component {
           <Column width={25}>
             <Section heading="Your Notes">
               <Button>Create a New Note</Button>
-              <hr />
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
-              <hr />
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
-              <hr />
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
-              <hr />
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+              <div className="note-excerpt note-excerpt--active">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit...</p>
+              </div>
+              <div className="note-excerpt">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit...</p>
+              </div>
+              <div className="note-excerpt">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit...</p>
+              </div>
             </Section>
           </Column>
 
           <Column width={75}>
             <Section heading="Note Content">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi magnam quis
-                voluptas. Sunt quo quam rerum, laudantium, asperiores temporibus optio eum ea fugiat
-                nam consequatur, sit nemo quisquam aspernatur illo?Et recusandae quam incidunt natus
-                placeat omnis porro, nihil perferendis, fugiat asperiores corrupti voluptatum eaque
-                aliquid dicta debitis officiis accusantium quae quod expedita quis vel itaque.
-                Sapiente repellat at minus.
+              <p className="editable-note-content" contentEditable>
+                Click anywhere in the box and begin typing to write your note...
               </p>
-
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi magnam quis
-                voluptas. Sunt quo quam rerum, laudantium, asperiores temporibus optio eum ea fugiat
-                nam consequatur, sit nemo quisquam aspernatur illo?Et recusandae quam incidunt natus
-                placeat omnis porro, nihil perferendis, fugiat asperiores corrupti voluptatum eaque
-                aliquid dicta debitis officiis accusantium quae quod expedita quis vel itaque.
-                Sapiente repellat at minus.
-              </p>
-
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi magnam quis
-                voluptas. Sunt quo quam rerum, laudantium, asperiores temporibus optio eum ea fugiat
-                nam consequatur, sit nemo quisquam aspernatur illo?Et recusandae quam incidunt natus
-                placeat omnis porro, nihil perferendis, fugiat asperiores corrupti voluptatum eaque
-                aliquid dicta debitis officiis accusantium quae quod expedita quis vel itaque.
-                Sapiente repellat at minus.
-              </p>
+              <Button formButton>Save Note</Button>
             </Section>
           </Column>
         </SheetsPageContainer>
