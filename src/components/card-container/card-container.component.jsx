d44848a7cddb9bf6413271d98484079505ca5fa1
Talon Durrant <talonmdurrant@gmail.com>
2021-01-25 01:39:48 -0700

    allowing subheadings to be blue for campaign sheet

--- src/components/card-container/card-container.component.jsx
+++ src/components/card-container/card-container.component.jsx
@@ -2,12 +2,18 @@ import React from "react"
 
 import "./card-container.styles.scss"
 
-const CardContainer = ({ children, heading, subheading, terheading }) => (
+const CardContainer = ({ children, heading, subheading, terheading, blue }) => (
   <div className="card-container">
     <h3 className="card-container__heading">{heading}</h3>
-    {subheading ? <h4 className="card-container__subheading">{subheading}</h4> : null}
+    {subheading ? (
+      <h4
+        className={`card-container__subheading ${blue ? "card-container__subheading--blue" : ""}`}
+      >
+        {subheading}
+      </h4>
+    ) : null}
     {terheading ? <h4 className="card-container__terheading">{terheading}</h4> : null}
-    {children}
+    <div className="card-container__content">{children}</div>
     {/* <EditDelete type="charsheet" /> */}
   </div>
 )
