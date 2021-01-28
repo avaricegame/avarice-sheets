2a1e040420379aae11a5f2ed4da8de50bfe5a7e2
Talon Durrant <talonmdurrant@gmail.com>
2021-01-27 20:25:25 -0700

    created and utilized global variables and made second version dark mode color variables for future use

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,16 @@
 import React from "react"
 
-import { CustomButtonContainer } from "./custom-button.styles.jsx"
+import "./custom-button.styles.scss"
 
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+const CustomButton = ({ children, formButton, signOut, ...props }) => (
+  <button
+    className={`custom-button ${formButton ? "custom-button--form-button" : ""} ${
+      signOut ? "custom-button--sign-out-button" : ""
+    }`}
+    {...props}
+  >
+    {children}
+  </button>
 )
 
 export default CustomButton
