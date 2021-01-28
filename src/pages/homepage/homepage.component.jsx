6b4009d0036f0b3e2c9235483067beb466354add
Talon Durrant <talonmdurrant@gmail.com>
2021-01-28 05:22:26 -0700

    hooked up the homepage to fetch all the data needed through redux

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import { connect } from "react-redux"
 import { createStructuredSelector } from "reselect"
 
@@ -9,12 +8,12 @@ import { default as Column } from "../../components/main-page-container-column/m
 import Footer from "../../components/footer/footer.component"
 
 import CharacterSheetCardsContainer from "../../components/character-sheet-cards/character-sheet-cards.container"
-import CampaignSheetCard from "../../components/campaign-sheet-card/campaign-sheet-card.component"
+import CampaignSheetCardsContainer from "../../components/campaign-sheet-cards/campaign-sheet-cards.container"
 
 import CustomButton from "../../components/custom-button/custom-button.component"
 
 import { fetchCharSheetListStartAsync } from "../../redux/character-sheet/character-sheet.actions"
-import { fetchCampSheetListStart } from "../../redux/campaign-sheet/campaign-sheet.actions"
+import { fetchCampSheetListStartAsync } from "../../redux/campaign-sheet/campaign-sheet.actions"
 import { selectCurrentUser } from "../../redux/user/user.selectors"
 
 import "./homepage.styles.scss"
@@ -23,12 +22,12 @@ class HomePage extends React.Component {
   componentDidMount() {
     const {
       fetchCharSheetListStartAsync,
-      fetchCampSheetListStart,
+      fetchCampSheetListStartAsync,
       currentUser: { id },
     } = this.props
 
     fetchCharSheetListStartAsync(id)
-    fetchCampSheetListStart()
+    fetchCampSheetListStartAsync(id)
   }
 
   render() {
@@ -40,18 +39,9 @@ class HomePage extends React.Component {
             <CustomButton>Create a New Character Sheet</CustomButton>
             <CharacterSheetCardsContainer />
           </Column>
-
           <Column heading="Campaign Sheets">
             <CustomButton>Create a New Campaign Sheet</CustomButton>
-            <Link className="sheets-card-link" to="/campaign">
-              <CampaignSheetCard />
-            </Link>
-            <Link className="sheets-card-link" to="/campaign">
-              <CampaignSheetCard />
-            </Link>
-            <Link className="sheets-card-link" to="/campaign">
-              <CampaignSheetCard />
-            </Link>
+            <CampaignSheetCardsContainer />
           </Column>
         </MainPageContainer>
         <Footer />
@@ -65,8 +55,10 @@ const mapStateToProps = createStructuredSelector({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCharSheetListStartAsync: (userid) => dispatch(fetchCharSheetListStartAsync(userid)),
-  fetchCampSheetListStart: (campSheetList) => dispatch(fetchCampSheetListStart(campSheetList)),
+  fetchCharSheetListStartAsync: (currentUsersID) =>
+    dispatch(fetchCharSheetListStartAsync(currentUsersID)),
+  fetchCampSheetListStartAsync: (currentUsersID) =>
+    dispatch(fetchCampSheetListStartAsync(currentUsersID)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
