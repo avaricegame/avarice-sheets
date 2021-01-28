ffb2a17c4aefaee78301952924f81a5d1376394a
Talon Durrant <talonmdurrant@gmail.com>
2021-01-27 17:20:16 -0700

    made the card container component much more versatile

--- src/components/card-container/card-container.component.jsx
+++ src/components/card-container/card-container.component.jsx
@@ -2,18 +2,30 @@ import React from "react"
 
 import "./card-container.styles.scss"
 
+export const InteriorCardContainerFlex = ({ children }) => (
+  <div className="card-container--flex">{children}</div>
+)
+
+export const Subheading = ({ children, blue }) => (
+  <h4 className={`card-container__subheading ${blue ? "card-container__subheading--blue" : ""}`}>
+    {children}
+  </h4>
+)
+
+export const Terheading = ({ children }) => (
+  <h4 className="card-container__terheading">{children}</h4>
+)
+
+export const CardContainerContentContainer = ({ children }) => (
+  <div className="card-container__content">{children}</div>
+)
+
 const CardContainer = ({ children, heading, subheading, terheading, blue }) => (
-  <div className="card-container">
+  <div className={`card-container`}>
     <h3 className="card-container__heading">{heading}</h3>
-    {subheading ? (
-      <h4
-        className={`card-container__subheading ${blue ? "card-container__subheading--blue" : ""}`}
-      >
-        {subheading}
-      </h4>
-    ) : null}
-    {terheading ? <h4 className="card-container__terheading">{terheading}</h4> : null}
-    <div className="card-container__content">{children}</div>
+    {subheading ? <Subheading blue={blue}>{subheading}</Subheading> : null}
+    {terheading ? <Terheading>{terheading}</Terheading> : null}
+    <CardContainerContentContainer>{children}</CardContainerContentContainer>
     {/* <EditDelete type="charsheet" /> */}
   </div>
 )
