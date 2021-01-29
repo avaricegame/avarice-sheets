7e9df18ac799f225c003e167a6e5ef3ef32286de
Talon Durrant <talonmdurrant@gmail.com>
2021-01-29 04:47:01 -0700

    built out the display wearables component

---
+++ src/components/character-sheet-components/display-wearables/display-wearables.component.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+
+import { default as Card } from "../../card-container/card-container.component"
+
+const DisplayWearables = ({ wearables }) => (
+  <>
+    {wearables.map((wearable) => {
+      const {
+        name,
+        id,
+        armourValue,
+        bodyArea,
+        description,
+        equipped,
+        statModifiers,
+        abilities,
+      } = wearable
+      return (
+        <Card
+          heading={name}
+          subheading={bodyArea}
+          terheading={`${equipped ? "Equipped" : ""}`}
+          key={id}
+        >
+          <p>
+            <strong>Armour Value: </strong>
+            {armourValue}
+          </p>
+          <table>
+            <thead>
+              <tr>
+                <th>Stat Modifiers:</th>
+              </tr>
+            </thead>
+            <tbody>
+              {statModifiers.map(({ modifier, value }) => {
+                if (value !== 0) {
+                  return (
+                    <tr key={modifier}>
+                      <td>{modifier}:</td>
+                      <td>{value}</td>
+                    </tr>
+                  )
+                } else {
+                  return null
+                }
+              })}
+            </tbody>
+          </table>
+          <p>
+            <strong>Description: </strong>
+            {description}
+          </p>
+          <p>
+            <strong>Abilities: </strong>
+            {abilities.length}
+          </p>
+        </Card>
+      )
+    })}
+  </>
+)
+
+export default DisplayWearables
