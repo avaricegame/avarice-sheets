05f5caa1083f61bbed1f1de1b6c2c525ab1dc3b5
Talon Durrant <talonmdurrant@gmail.com>
2021-01-28 19:09:37 -0700

    set up fetching a user to be an async redux action

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,37 @@
 import UserActionTypes from "./user.types"
 
-export const setCurrentUser = (user) => ({
-  type: UserActionTypes.SET_CURRENT_USER,
-  payload: user,
+export const fetchCurrentUserStart = () => ({
+  type: UserActionTypes.FETCH_CURRENT_USER_START,
 })
+
+export const fetchCurrentUserSuccess = (currentUser) => ({
+  type: UserActionTypes.FETCH_CURRENT_USER_SUCCESS,
+  payload: currentUser,
+})
+
+export const fetchCurrentUserFailure = (errorMessage) => ({
+  type: UserActionTypes.FETCH_CURRENT_USER_FAILURE,
+  payload: errorMessage,
+})
+
+export const fetchCurrentUserStartAsync = (userNameOrEmail, userPassword) => {
+  return (dispatch) => {
+    const USERS = require("../json/users.json")
+
+    const requestedUser = USERS.find(
+      (user) => user.email === userNameOrEmail || user.username === userNameOrEmail
+    )
+
+    if (!requestedUser) {
+      return dispatch(
+        fetchCurrentUserFailure("There is no account associated with those credentials.")
+      )
+    }
+
+    if (requestedUser.password === userPassword) {
+      return dispatch(fetchCurrentUserSuccess(requestedUser))
+    }
+
+    return dispatch(fetchCurrentUserFailure("Provided credentials do not match."))
+  }
+}
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -2,14 +2,29 @@ import UserActionTypes from "./user.types"
 
 const INITIAL_STATE = {
   currentUser: null,
+  isUserFetching: false,
+  errorMessage: null,
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case UserActionTypes.SET_CURRENT_USER:
+    case UserActionTypes.FETCH_CURRENT_USER_START:
       return {
         ...state,
+        isUserFetching: true,
+      }
+    case UserActionTypes.FETCH_CURRENT_USER_SUCCESS:
+      return {
+        ...state,
+        isUserFetching: false,
+        errorMessage: undefined,
         currentUser: action.payload,
+      }
+    case UserActionTypes.FETCH_CURRENT_USER_FAILURE:
+      return {
+        ...state,
+        isUserFetching: false,
+        errorMessage: action.payload,
       }
     default:
       return state
