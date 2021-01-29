b6965229b0f6095927e93dca4c59d9d14b535b8d
Talon Durrant <talonmdurrant@gmail.com>
2021-01-28 22:54:09 -0700

    hooked campaign page up to redux and set it up to set the current campaign sheet

--- src/pages/campaign-sheet/campaign-sheet.component.jsx
+++ src/pages/campaign-sheet/campaign-sheet.component.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Switch, Route, Redirect } from "react-router-dom"
+import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect"
 
-import SheetsHeader from "../../components/headers/sheets-header.component"
+import { default as CampaignSheetHeader } from "../../components/headers/campaign-sheet-header.container"
 import CampaignSheetNavigation from "../../components/sheets-navigation/campaign-sheet-navigation.component"
 import Footer from "../../components/footer/footer.component"
 
-import CampaignGameplay from "./campaign-sheet-pages/campaign-gameplay.component"
+import CampaignGameplayPage from "./campaign-sheet-pages/campaign-gameplay.component"
 import CombatPage from "./campaign-sheet-pages/combat.component"
 import PlayersPage from "./campaign-sheet-pages/players.component"
 import CampaignPage from "./campaign-sheet-pages/campaign.component"
@@ -15,28 +17,36 @@ import NotesPage from "../sheets-pages/notes/notes.components"
 
 import SheetsPageNotFound from "../sheets-page-not-found/sheets-page-not-found.component"
 
+import { selectCurrentUser } from "../../redux/user/user.selectors"
+import { fetchCurrentCampSheetByIDStartAsync } from "../../redux/campaign-sheet/campaign-sheet.actions"
+
 class CampaignSheetPage extends React.Component {
+  componentDidMount() {
+    const { fetchCurrentCampSheetByIDStartAsync, match, currentUser } = this.props
+    fetchCurrentCampSheetByIDStartAsync(match.params.campid, currentUser)
+  }
+
   render() {
     const { match } = this.props
     return (
       <>
-        <SheetsHeader campaign name="Campaign Name" />
+        <CampaignSheetHeader />
         <CampaignSheetNavigation />
         <Switch>
           <Route
             exact
-            path={`${match.path}`}
-            render={() => <Redirect to={`${match.path}/gameplay`} />}
+            path={`${match.url}`}
+            render={() => <Redirect to={`${match.url}/gameplay`} />}
           />
-          <Route exact path={`${match.path}/gameplay`} component={CampaignGameplay} />
-          <Route exact path={`${match.path}/combat`} component={CombatPage} />
-          <Route exact path={`${match.path}/players`} component={PlayersPage} />
-          <Route exact path={`${match.path}/campaign`} component={CampaignPage} />
-          <Route exact path={`${match.path}/planning`} component={PlanningPage} />
-          <Route exact path={`${match.path}/messages`} component={MessagesPage} />
-          <Route exact path={`${match.path}/notes`} component={NotesPage} />
+          <Route exact path={`${match.url}/gameplay`} component={CampaignGameplayPage} />
+          <Route exact path={`${match.url}/combat`} component={CombatPage} />
+          <Route exact path={`${match.url}/players`} component={PlayersPage} />
+          <Route exact path={`${match.url}/campaign`} component={CampaignPage} />
+          <Route exact path={`${match.url}/planning`} component={PlanningPage} />
+          <Route exact path={`${match.url}/messages`} component={MessagesPage} />
+          <Route exact path={`${match.url}/notes`} component={NotesPage} />
 
-          <Route path={`${match.path}`} component={SheetsPageNotFound} />
+          <Route path={`${match.url}`} component={SheetsPageNotFound} />
         </Switch>
         <Footer />
       </>
@@ -44,4 +54,13 @@ class CampaignSheetPage extends React.Component {
   }
 }
 
-export default CampaignSheetPage
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchCurrentCampSheetByIDStartAsync: (campSheetIDParam, currentUser) =>
+    dispatch(fetchCurrentCampSheetByIDStartAsync(campSheetIDParam, currentUser)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CampaignSheetPage)
