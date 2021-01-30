2dc3e8bbf9f97fe7a7e6709385490f1f193835a4
Talon Durrant <talonmdurrant@gmail.com>
2021-01-30 01:39:58 -0700

    rewrote and sorted through functions to make sure they were understandable and bug-free

--- src/pages/character-sheet-pages/utils/stats.utils.js
+++ src/pages/character-sheet-pages/utils/stats.utils.js
@@ -1,149 +1,137 @@
 // this lets me easily get a proficiency value from the stats
 // without having to go digging around in the stats object on
 // any particular page
-export const findStatProficiencyValue = (stats, statName) => {
-  let proficiencyValue
+export const findStatProficiencyValue = (calculatedStats, statName) => {
+  const requestedStat = calculatedStats.find(
+    (statObj) => statObj.name.toUpperCase() === statName.toUpperCase()
+  )
 
-  switch (statName.toUpperCase()) {
-    case "STRENGTH":
-      proficiencyValue = stats[0].proficiencyPoints
-      break
-    case "CONSTITUTION":
-      proficiencyValue = stats[1].proficiencyPoints
-      break
-    case "REFLEX":
-      proficiencyValue = stats[2].proficiencyPoints
-      break
-    case "SPEED":
-      proficiencyValue = stats[3].proficiencyPoints
-      break
-    case "TECHNICAL":
-      proficiencyValue = stats[4].proficiencyPoints
-      break
-    case "PRACTICAL":
-      proficiencyValue = stats[5].proficiencyPoints
-      break
-    case "SPEECH":
-      proficiencyValue = stats[6].proficiencyPoints
-      break
-    case "PERFORM":
-      proficiencyValue = stats[7].proficiencyPoints
-      break
-    case "RANGED":
-      proficiencyValue = stats[8].proficiencyPoints
-      break
-    case "MELEE":
-      proficiencyValue = stats[9].proficiencyPoints
-      break
-    default:
-      break
-  }
+  const proficiencyValue = requestedStat.proficiencyPoints
 
   return proficiencyValue
 }
-
-export const calculateActualStatValues = (stats, equippedWearables, classStats) => {}
 
 // this takes the stat values from the three sources: stats, equipped wearables
 // stats, and the class stats, and combines them all into one data source that
 // i can use to display on the app
 export const calculateActualStatValuesAndTransform = (stats, equippedWearables, classStats) => {
-  stats.forEach((statObj) => transformStatValuesObjects(statObj))
+  // deep copy the three passed args to remove ALL references to the original in memory
+  let copiedStats = JSON.parse(JSON.stringify(stats))
+  let copiedEquippedWearables = JSON.parse(JSON.stringify(equippedWearables))
+  let copiedClassStats = JSON.parse(JSON.stringify(classStats))
 
-  const equippedWearablesStats = equippedWearables.map((wearable) => wearable.statModifiers)
+  copiedStats.forEach((statObj) => transformStatValuesObjects(statObj))
 
-  equippedWearablesStats.forEach((wearablesArray) => {
-    wearablesArray.forEach((statObj) => transformEquippedWearablesStatValuesObjects(statObj))
+  // strip down the array of equipped wearables to be just an array of their stat arrays
+  const equippedWearablesStats = copiedEquippedWearables.map((wearable) => wearable.statModifiers)
+
+  equippedWearablesStats.forEach((statsArray) => {
+    statsArray.forEach((statObj) => transformOtherStatObjects(statObj, "A"))
   })
 
-  classStats.forEach((classStatsObj) => {
-    transformClassesStatValuesObjects(classStatsObj)
+  copiedClassStats.forEach((statObj) => {
+    transformOtherStatObjects(statObj, "C")
   })
 
-  addInEquippedWearablesStats(stats, equippedWearablesStats)
+  // loop through the array to send each nested
+  // array to be added to the main array
+  equippedWearablesStats.forEach((statsArray) => {
+    addStatsToMainArray(copiedStats, statsArray)
+  })
 
-  addInClassesStats(stats, classStats)
+  addStatsToMainArray(copiedStats, copiedClassStats, true)
 
-  return stats
+  return copiedStats
 }
 
 // the stat object proficiency values are stored as numbers
 // so this takes those objects, and adds a new array to them
 // the new array has the same number of items as the number
 // value of the proficiency, and those items are all the
-// bool, false
-const transformStatValuesObjects = (statValueObject) => {
+// bool, false, to know they came from the main stats array
+const transformStatValuesObjects = (statObject) => {
   let newArray = []
-  statValueObject.newProficiencyPoints = statValueObject.proficiencyPoints
+  statObject.newProficiencyPoints = statObject.proficiencyPoints
 
-  while (statValueObject.newProficiencyPoints > 0) {
+  while (statObject.newProficiencyPoints > 0) {
     newArray.push(false)
-    statValueObject.proficiencyValue += 1
-    statValueObject.newProficiencyPoints -= 1
+    statObject.newProficiencyPoints -= 1
   }
 
-  statValueObject.newProficiencyPoints = newArray
+  statObject.newProficiencyPoints = newArray
 }
 
 // the stat object proficiency values are stored as numbers
-// so this takes those objects, and adds a new array to them
+// so this takes those objects, and adds a new array to them.
 // the new array has the same number of items as the number
-// value of the proficiency, and those items are all the
-// bool, true, to let me know they are from wearables
-const transformEquippedWearablesStatValuesObjects = (wearableStatValueObject) => {
+// value of the proficiency (except an X for a negative number--
+// which will be resolved later)
+// and those items are all given a letter value, to be used in
+// the UI to let the user know where they came from
+const transformOtherStatObjects = (statObject, letterToPush) => {
   let newArray = []
-  wearableStatValueObject.newValue = wearableStatValueObject.value
+  statObject.newValue = statObject.value
 
-  while (wearableStatValueObject.newValue > 0) {
-    newArray.push("A")
-    wearableStatValueObject.value += 1
-    wearableStatValueObject.newValue -= 1
+  // check for subtracted stat modifications
+  // if there are, then push X onto the array
+  if (statObject.newValue < 0) {
+    while (statObject.newValue < 0) {
+      newArray.push("X")
+      statObject.newValue += 1
+    }
   }
 
-  wearableStatValueObject.newValue = newArray
-}
-
-// the stat object proficiency values are stored as numbers
-// so this takes those objects, and adds a new array to them
-// the new array has the same number of items as the number
-// value of the proficiency, and those items are all the
-// value, "C", to let me know they are from the class
-const transformClassesStatValuesObjects = (classesStatValueObject) => {
-  let newArray = []
-  classesStatValueObject.newValue = classesStatValueObject.value
-
-  while (classesStatValueObject.newValue > 0) {
-    newArray.push("C")
-    classesStatValueObject.value += 1
-    classesStatValueObject.newValue -= 1
+  while (statObject.newValue > 0) {
+    newArray.push(letterToPush)
+    statObject.newValue -= 1
   }
 
-  classesStatValueObject.newValue = newArray
+  console.log(statObject)
+  console.log(newArray)
+
+  statObject.newValue = newArray
 }
 
 // this takes the wearableStats array of arrays and loops through it,
-// then finds the associated object from the main stats array, and
-// adds the extra stats to that array
-const addInEquippedWearablesStats = (stats, wearableStats) => {
-  wearableStats.forEach((statsArray) => {
-    statsArray.forEach((statsObj) => {
-      const objToAddTo = stats.find(
-        (obj) => obj.name.toUpperCase() === statsObj.modifier.toUpperCase()
-      )
-      objToAddTo.newProficiencyPoints = [...objToAddTo.newProficiencyPoints, ...statsObj.newValue]
-      objToAddTo.proficiencyPoints = objToAddTo.proficiencyPoints + statsObj.value
-    })
-  })
-}
-
-// then finds the associated object from the main stats array of
-// objects, and adds the extra stats to that array
-const addInClassesStats = (stats, classStats) => {
-  classStats.forEach((statsObj) => {
+// then loops through each of its nested arrays, then finds the
+// associated object from the main stats array, and adds the
+// extra stats to that array
+const addStatsToMainArray = (stats, statsToAdd, addToBeginning) => {
+  statsToAdd.forEach((statsToAddObj) => {
     const objToAddTo = stats.find(
-      (obj) => obj.name.toUpperCase() === statsObj.modifier.toUpperCase()
+      (obj) => obj.name.toUpperCase() === statsToAddObj.modifier.toUpperCase()
     )
-    objToAddTo.newProficiencyPoints = [...statsObj.newValue, ...objToAddTo.newProficiencyPoints]
-    objToAddTo.proficiencyPoints = objToAddTo.proficiencyPoints + statsObj.value
+
+    console.log(objToAddTo)
+    console.log(statsToAddObj)
+
+    // if there were any subtracted stat modifications
+    // and X was push onto the array, then
+    // REMOVE the last element of the main array
+    // then POP the X off the array and PUSH it
+    // on to the end -- basically, the X is replacing
+    // the last element of the main array if there is an
+    // X at all
+    if (statsToAddObj.newValue.includes("X")) {
+      objToAddTo.newProficiencyPoints.pop()
+      objToAddTo.newProficiencyPoints.push(statsToAddObj.newValue.pop())
+    }
+
+    // if add to beginning is true, add the new array
+    // to the beginning instead of the end
+    if (addToBeginning) {
+      objToAddTo.newProficiencyPoints = [
+        ...statsToAddObj.newValue,
+        ...objToAddTo.newProficiencyPoints,
+      ]
+    } else {
+      objToAddTo.newProficiencyPoints = [
+        ...objToAddTo.newProficiencyPoints,
+        ...statsToAddObj.newValue,
+      ]
+    }
+
+    // add the actual numbers together for later use
+    objToAddTo.proficiencyPoints += statsToAddObj.value
   })
 }
