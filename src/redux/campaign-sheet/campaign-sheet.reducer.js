f1bd802dabe2da80c2ded5d6b4f4c617fe6e4da8
Talon Durrant <talonmdurrant@gmail.com>
2021-01-31 02:22:50 -0700

    if i ever need this version

--- src/redux/campaign-sheet/campaign-sheet.reducer.js
+++ src/redux/campaign-sheet/campaign-sheet.reducer.js
@@ -1,4 +1,7 @@
 import CampSheetActionTypes from "./campaign-sheet.types"
+import CampPageActionTypes from "./pages/pages.types"
+
+import { setCurrentMission } from "./utils/missions.utils"
 
 const INITIAL_STATE = {
   currentCampSheet: null,
@@ -9,6 +12,23 @@ const INITIAL_STATE = {
   doesCampaignSheetExist: true,
   doesUserHavePermission: true,
   arePlayersCharSheetsLoaded: true,
+  background: null,
+  campaignName: null,
+  captainsLog: null,
+  characterSheetsIDS: null,
+  charactersNames: null,
+  creatorID: null,
+  creatorName: null,
+  environment: null,
+  inventoryItems: null,
+  missions: null,
+  notes: null,
+  npcs: null,
+  personalLog: null,
+  players: null,
+  playersIDS: null,
+  playersNames: null,
+  _id: null,
 }
 
 const campSheetReducer = (state = INITIAL_STATE, action) => {
@@ -42,6 +62,7 @@ const campSheetReducer = (state = INITIAL_STATE, action) => {
         isCampSheetFetching: true,
       }
     case CampSheetActionTypes.FETCH_CURRENT_CAMP_SHEET_BY_ID_SUCCESS:
+      console.log(action.payload)
       return {
         ...state,
         isCampSheetFetching: false,
@@ -50,6 +71,23 @@ const campSheetReducer = (state = INITIAL_STATE, action) => {
         doesUserHavePermission: true,
         arePlayersCharSheetsLoaded: true,
         currentCampSheet: action.payload,
+        background: action.payload.background,
+        campaignName: action.payload.campaignName,
+        captainsLog: action.payload.captainsLog,
+        characterSheetsIDS: action.payload.characterSheetsIDS,
+        charactersNames: action.payload.charactersNames,
+        creatorID: action.payload.creatorID,
+        creatorName: action.payload.creatorName,
+        environment: action.payload.environment,
+        inventoryItems: action.payload.inventoryItems,
+        missions: action.payload.missions,
+        notes: action.payload.notes,
+        npcs: action.payload.npcs,
+        personalLog: action.payload.personalLog,
+        players: action.payload.players,
+        playersIDS: action.payload.playersIDS,
+        playersNames: action.payload.playersNames,
+        _id: action.payload._id,
       }
     case CampSheetActionTypes.FETCH_CURRENT_CAMP_SHEET_BY_ID_FAILURE_NONE_FOUND:
       console.log(action.payload)
@@ -74,6 +112,15 @@ const campSheetReducer = (state = INITIAL_STATE, action) => {
         isCampSheetFetching: false,
         errorMessage: action.payload,
         arePlayersCharSheetsLoaded: false,
+      }
+    case CampSheetActionTypes.SET_CURRENT_MISSION:
+      console.log(action.payload)
+      return {
+        ...state,
+        currentCampSheet: {
+          ...state.currentCampSheet,
+          missions: setCurrentMission(state.currentCampSheet.missions, action.payload),
+        },
       }
     default:
       return state
