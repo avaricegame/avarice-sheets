4c92c3ff7ae33391cce8916188600019da612c1c
Talon Durrant <talonmdurrant@gmail.com>
2021-02-01 21:20:53 -0700

    finished the level up form

--- src/components/popup-form/forms/character-sheet/level-up.form.jsx
+++ src/components/popup-form/forms/character-sheet/level-up.form.jsx
@@ -7,67 +7,142 @@ import "../forms.styles.scss"
 // display components
 import { PopupFormHeading } from "../../popup-form.component"
 import { default as ButtonPanel } from "../../../popup-form-button-panel/popup-form-button-panel.component"
-import { default as Button } from "../../../custom-button/custom-button.component"
 
 // selectors
 import {
-  selectStats,
-  selectWearables,
-  selectRaceInfo,
-} from "../../../../redux/character-sheet/character-sheet.selectors"
+  Subheading,
+  CardContainerContentContainer,
+} from "../../../card-container/card-container.component"
 
-// util functions
-import { calculateActualStatValuesAndTransform } from "../../../../pages/character-sheet-pages/utils/stats.utils"
-import { findEquippedInventoryItems } from "../../../../pages/character-sheet-pages/utils/inventory.utils"
-import {
-  mapDifficultyToValueToBeat,
-  findStatBeingChecked,
-  addOrSubtractAdvantageToValueToBeat,
-  determineAdvantageBonus,
-} from "../../utils/make-a-check.utils"
+// selectors
+import { selectAbilities } from "../../../../redux/character-sheet/character-sheet.selectors"
 
 // actions
 import { makeACheck } from "../../../../redux/character-sheet/pages/pages.actions"
 
-class MakeACheck extends React.Component {
+class LevelUp extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      transformedCalculatedStatValues: [],
-      advantage: 0,
-      difficulty: null,
+      abilityToUnlockID: null,
+      abilityToUnlock: {
+        ability1: {
+          id: null,
+          type: null,
+        },
+        ability2: {
+          id: null,
+          type: null,
+        },
+        ability3: {
+          id: null,
+          type: null,
+        },
+        ability4: {
+          id: null,
+          type: null,
+        },
+        ability5: {
+          id: null,
+          type: null,
+        },
+      },
     }
   }
 
-  componentDidMount() {
-    const { stats, wearables, raceInfo } = this.props
-
+  unlockAbility(id, type) {
+    const newAbilityToUnlock = {
+      ability1: {
+        id: null,
+        type: null,
+      },
+      ability2: {
+        id: null,
+        type: null,
+      },
+      ability3: {
+        id: null,
+        type: null,
+      },
+      ability4: {
+        id: null,
+        type: null,
+      },
+      ability5: {
+        id: null,
+        type: null,
+      },
+    }
     this.setState({
-      transformedCalculatedStatValues: calculateActualStatValuesAndTransform(
-        stats,
-        findEquippedInventoryItems(wearables),
-        raceInfo.stats
-      ),
+      abilityToUnlockID: id,
+      abilityToUnlock: { ...newAbilityToUnlock, [`ability${id}`]: { id, type } },
     })
   }
 
-  makeCheck(type) {}
+  handleSubmit(e) {
+    e.preventDefault()
+    const { abilityToUnlock, abilityToUnlockID } = this.state
 
-  componentWillUnmount() {
-    this.setState({
-      transformedCalculatedStatValues: [],
-    })
+    if (!abilityToUnlockID) return window.alert("You must select an ability to unlock.")
+
+    window.alert("This form action has not been set up yet.")
+    console.log(abilityToUnlock)
   }
 
   render() {
+    const {
+      abilities: { abilities },
+    } = this.props
+    const { abilityToUnlock } = this.state
     return (
       <>
         <PopupFormHeading>Level Up</PopupFormHeading>
-        <form className="popupform__form purple-top-border">
-          <fieldset></fieldset>
+        <form className="popupform__form purple-top-border" onSubmit={(e) => this.handleSubmit(e)}>
+          <fieldset>
+            <label htmlFor="unlock-an-ability">Please Select an Ability to Unlock:</label>
+            {abilities.map((ability) => {
+              return (
+                <div className="level-up-form__abilities-overview" key={ability.id}>
+                  <Subheading>{ability.name}</Subheading>
+                  {ability.types.map((abilityType) => {
+                    const { difficulty, associatedStat, description, type, unlocked } = abilityType
+                    return (
+                      <div key={type}>
+                        {!unlocked ? (
+                          <div
+                            className={`level-up-form__abilities ${
+                              abilityToUnlock[`ability${ability.id}`].type === type
+                                ? "level-up-form__abilities--selected"
+                                : "level-up-form__abilities--locked"
+                            }`}
+                            onClick={() => this.unlockAbility(ability.id, type)}
+                          >
+                            <CardContainerContentContainer>
+                              <p>{type.toUpperCase()}</p>
+                              <ul>
+                                <li>
+                                  <em>Difficulty: </em>
+                                  {difficulty}
+                                </li>
+                                <li>
+                                  <em>Associated Stat: </em>
+                                  {associatedStat}
+                                </li>
+                              </ul>
+                              <p>{description}</p>
+                            </CardContainerContentContainer>
+                          </div>
+                        ) : null}
+                      </div>
+                    )
+                  })}
+                </div>
+              )
+            })}
+          </fieldset>
 
-          <ButtonPanel submitValue={`Submit`} />
+          <ButtonPanel submitValue={`Level Up`} />
         </form>
       </>
     )
@@ -75,13 +150,11 @@ class MakeACheck extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  stats: selectStats,
-  wearables: selectWearables,
-  raceInfo: selectRaceInfo,
+  abilities: selectAbilities,
 })
 
 const mapDispatchToProps = (dispatch) => ({
   makeACheck: (typeAndSuccess) => dispatch(makeACheck(typeAndSuccess)),
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(MakeACheck)
+export default connect(mapStateToProps, mapDispatchToProps)(LevelUp)
