2a4457e84b7ff3e3ef31f0c15a60fb0f09f3db43
Talon Durrant <talonmdurrant@gmail.com>
2021-02-02 12:26:25 -0700

    created sell inventory items form

--- src/components/popup-form/forms/character-sheet/sell-inventory-item.form.jsx
+++ src/components/popup-form/forms/character-sheet/sell-inventory-item.form.jsx
@@ -7,76 +7,180 @@ import "../forms.styles.scss"
 // display components
 import { PopupFormHeading } from "../../popup-form.component"
 import { default as ButtonPanel } from "../../../popup-form-button-panel/popup-form-button-panel.component"
-import { default as Button } from "../../../custom-button/custom-button.component"
+import WearableCard from "../../../shared-sheets-components/wearable-card/wearable-card.component"
+import WeaponCard from "../../../shared-sheets-components/weapon-card/weapon-card.component"
+import ItemCard from "../../../shared-sheets-components/item-card/item-card.component"
 
 // selectors
 import {
-  selectStats,
+  selectWeapons,
   selectWearables,
-  selectRaceInfo,
+  selectItems,
 } from "../../../../redux/character-sheet/character-sheet.selectors"
-
-// util functions
-import { calculateActualStatValuesAndTransform } from "../../../../pages/character-sheet-pages/utils/stats.utils"
-import { findEquippedInventoryItems } from "../../../../pages/character-sheet-pages/utils/inventory.utils"
-import {
-  mapDifficultyToValueToBeat,
-  findStatBeingChecked,
-  addOrSubtractAdvantageToValueToBeat,
-  determineAdvantageBonus,
-} from "../../utils/make-a-check.utils"
 
 // actions
 import { makeACheck } from "../../../../redux/character-sheet/pages/pages.actions"
 
-class MakeACheck extends React.Component {
+class AddNewWearable extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      transformedCalculatedStatValues: [],
-      advantage: 0,
-      difficulty: null,
+      displayInventoryItem: false,
+      type: "PLEASE SPECIFY",
+      value: 0,
+      id: null,
+      currentWeapon: null,
+      currentItem: null,
+      currentWearable: null,
     }
   }
 
-  componentDidMount() {
-    const { stats, wearables, raceInfo } = this.props
+  handleChange(e) {
+    const { wearables, weapons, items } = this.props
+    const { type } = this.state
 
-    this.setState({
-      transformedCalculatedStatValues: calculateActualStatValuesAndTransform(
-        stats,
-        findEquippedInventoryItems(wearables),
-        raceInfo.stats
-      ),
-    })
+    if (e.target.value !== "SELECT ONE") {
+      let currentInventoryItem
+
+      if (type === "WEAPON") {
+        currentInventoryItem = weapons.find((weapon) => weapon.id === e.target.value)
+        console.log(currentInventoryItem)
+        this.setState({
+          currentWeapon: currentInventoryItem,
+          displayInventoryItem: true,
+          id: currentInventoryItem.id,
+        })
+      }
+
+      if (type === "ITEM") {
+        currentInventoryItem = items.find((item) => item.id === e.target.value)
+        this.setState({
+          currentItem: currentInventoryItem,
+          displayInventoryItem: true,
+          id: currentInventoryItem.id,
+        })
+      }
+
+      if (type === "WEARABLE") {
+        currentInventoryItem = wearables.find((wearable) => wearable.id === e.target.value)
+        this.setState({
+          currentWearable: currentInventoryItem,
+          displayInventoryItem: true,
+          id: currentInventoryItem.id,
+        })
+      }
+    } else {
+      this.setState({
+        displayInventoryItem: false,
+      })
+    }
   }
 
-  makeCheck(type) {}
+  handleSubmit(e) {
+    e.preventDefault()
+    const { value, type, currentInventoryItem } = this.state
 
-  componentWillUnmount() {
-    this.setState({
-      transformedCalculatedStatValues: [],
-    })
+    if (value === "") return window.alert("You must specify a value in order to sell.")
+
+    window.alert("Form is not hooked up yet")
+    console.log(value, type, currentInventoryItem)
   }
 
   render() {
+    const { wearables, weapons, items } = this.props
+    const {
+      value,
+      displayInventoryItem,
+      currentWeapon,
+      currentItem,
+      currentWearable,
+      type,
+    } = this.state
+
     return (
       <>
         <PopupFormHeading>Sell Inventory Item</PopupFormHeading>
-        <form className="popupform__form purple-top-border">
+        <form className="popupform__form purple-top-border" onSubmit={(e) => this.handleSubmit(e)}>
           <fieldset>
-            <label htmlFor="difficulty">Please Choose an Inventory Item to Sell</label>
-            <select name="difficulty">
-              <option value={null}>Select One</option>
-              <option value="RISK">Item 1</option>
-              <option value={1}>Item 2</option>
+            <label htmlFor="choose-something">Inventory Type</label>
+            <select
+              name="choose-something"
+              onChange={(e) => this.setState({ type: e.target.value })}
+            >
+              <option value="PLEASE SPECIFY">Please Specify</option>
+              <option value="WEAPON">Weapon</option>
+              <option value="ITEM">Item</option>
+              <option value="WEARABLE">Wearable</option>
             </select>
 
-            {/* <ItemCard /> */}
-          </fieldset>
+            {type === "WEAPON" ? (
+              <>
+                <label htmlFor="choose-weapon">Choose A Weapon to Sell</label>
+                <select name="choose-weapon" onChange={(e) => this.handleChange(e, "WEAPON")}>
+                  <option value="SELECT ONE">Select a Weapon</option>
+                  {weapons.map(({ name, id }, index) => (
+                    <option key={index} value={id}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+              </>
+            ) : null}
 
-          <ButtonPanel submitValue={`Please Select`} />
+            {type === "ITEM" ? (
+              <>
+                <label htmlFor="choose-item">Choose An Item to Sell</label>
+                <select name="choose-item" onChange={(e) => this.handleChange(e, "ITEM")}>
+                  <option value="SELECT ONE">Select an Item</option>
+                  {items.map(({ name, id }, index) => (
+                    <option key={index} value={id}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+              </>
+            ) : null}
+
+            {type === "WEARABLE" ? (
+              <>
+                <label htmlFor="choose-wearable">Choose A Wearable to Sell</label>
+                <select name="choose-wearable" onChange={(e) => this.handleChange(e, "WEARABLE")}>
+                  <option value="SELECT ONE">Select a Wearable</option>
+                  {wearables.map(({ name, id }, index) => (
+                    <option key={index} value={id}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+              </>
+            ) : null}
+
+            {type !== "PLEASE SPECIFY" ? (
+              <>
+                <label htmlFor="value">Value of {type || "Inventory Item"}</label>
+                <input
+                  type="number"
+                  name="value"
+                  value={value}
+                  onChange={(e) => this.setState({ value: e.target.value })}
+                />
+              </>
+            ) : null}
+
+            {displayInventoryItem && type === "WEAPON" && currentWeapon ? (
+              <WeaponCard weapon={currentWeapon} />
+            ) : null}
+
+            {displayInventoryItem && type === "ITEM" && currentItem ? (
+              <ItemCard item={currentItem} />
+            ) : null}
+
+            {displayInventoryItem && type === "WEARABLE" && currentWearable ? (
+              <WearableCard wearable={currentWearable} />
+            ) : null}
+          </fieldset>
+          <ButtonPanel submitValue={`${type === "PLEASE SPECIFY" ? type : `Sell ${type}`}`} />
         </form>
       </>
     )
@@ -84,13 +188,13 @@ class MakeACheck extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  stats: selectStats,
+  weapons: selectWeapons,
+  items: selectItems,
   wearables: selectWearables,
-  raceInfo: selectRaceInfo,
 })
 
 const mapDispatchToProps = (dispatch) => ({
   makeACheck: (typeAndSuccess) => dispatch(makeACheck(typeAndSuccess)),
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(MakeACheck)
+export default connect(mapStateToProps, mapDispatchToProps)(AddNewWearable)
