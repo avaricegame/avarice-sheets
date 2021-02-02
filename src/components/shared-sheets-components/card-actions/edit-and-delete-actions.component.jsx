2fe4b939c49da380bcd565678c8c0a7ceb9a53ea
Talon Durrant <talonmdurrant@gmail.com>
2021-02-02 08:48:41 -0700

    buffed up the edit and delete component to also do copying and moving missions

--- src/components/shared-sheets-components/card-actions/edit-and-delete-actions.component.jsx
+++ src/components/shared-sheets-components/card-actions/edit-and-delete-actions.component.jsx
@@ -2,10 +2,45 @@ import React from "react"
 import { connect } from "react-redux"
 
 import { togglePopupFormForEdit } from "../../../redux/app/app.actions"
+import PopupFormTypes from "../../popup-form/popup-form.types"
 
-const EditAndDeleteActions = ({ whatToEdit, objToEdit, togglePopupFormForEdit, popupFormType }) => {
+const EditAndDeleteActions = ({
+  whatToEdit,
+  objToEdit,
+  togglePopupFormForEdit,
+  popupFormType,
+  moveEdit,
+}) => {
   return (
     <p className="actions">
+      {moveEdit ? (
+        <>
+          <span
+            onClick={() =>
+              togglePopupFormForEdit({
+                popupFormType: PopupFormTypes.COPY_INTERACTABLE_TO_DIFFERENT_MISSION,
+                whatToEdit: whatToEdit,
+                objToEdit: objToEdit,
+              })
+            }
+          >
+            COPY
+          </span>{" "}
+          |{" "}
+          <span
+            onClick={() =>
+              togglePopupFormForEdit({
+                popupFormType: PopupFormTypes.MOVE_INTERACTABLE_TO_DIFFERENT_MISSION,
+                whatToEdit: whatToEdit,
+                objToEdit: objToEdit,
+              })
+            }
+          >
+            MOVE
+          </span>{" "}
+          |{" "}
+        </>
+      ) : null}
       <span
         onClick={() =>
           togglePopupFormForEdit({
